110fac5cadfee6d03196c0d58ab7981318ff2419
HuyPoti <huynguyengiapoti@gmail.com>
2025-04-24 18:04:48 +0700

    change data to engine

---
+++ assets/tilesets/Rocks_03.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Rocks_03" tilewidth="128" tileheight="128" tilecount="8" columns="8">
+ <image source="../sprites/Terrain/Water/Rocks/Rocks_03.png" width="1024" height="128"/>
+</tileset>
