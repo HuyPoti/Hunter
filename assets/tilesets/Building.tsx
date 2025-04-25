210489048c9a9724cde1846c8b4b9cb1434b5975
HuyPoti <huynguyengiapoti@gmail.com>
2025-04-25 21:35:43 +0700

    config 5

---
+++ assets/tilesets/Building.tsx
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Building" tilewidth="320" tileheight="256" tilecount="18" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../sprites/Factions/Knights/Buildings/Castle/Castle_Blue.png" width="320" height="256"/>
+ </tile>
+ <tile id="1">
+  <image source="../sprites/Factions/Knights/Buildings/Castle/Castle_Construction.png" width="320" height="256"/>
+ </tile>
+ <tile id="2">
+  <image source="../sprites/Factions/Knights/Buildings/Castle/Castle_Destroyed.png" width="320" height="256"/>
+ </tile>
+ <tile id="3">
+  <image source="../sprites/Factions/Knights/Buildings/Castle/Castle_Purple.png" width="320" height="256"/>
+ </tile>
+ <tile id="4">
+  <image source="../sprites/Factions/Knights/Buildings/Castle/Castle_Red.png" width="320" height="256"/>
+ </tile>
+ <tile id="5">
+  <image source="../sprites/Factions/Knights/Buildings/Castle/Castle_Yellow.png" width="320" height="256"/>
+ </tile>
+ <tile id="6">
+  <image source="../sprites/Factions/Knights/Buildings/House/House_Blue.png" width="128" height="192"/>
+ </tile>
+ <tile id="7">
+  <image source="../sprites/Factions/Knights/Buildings/House/House_Construction.png" width="128" height="192"/>
+ </tile>
+ <tile id="8">
+  <image source="../sprites/Factions/Knights/Buildings/House/House_Destroyed.png" width="128" height="192"/>
+ </tile>
+ <tile id="9">
+  <image source="../sprites/Factions/Knights/Buildings/House/House_Purple.png" width="128" height="192"/>
+ </tile>
+ <tile id="10">
+  <image source="../sprites/Factions/Knights/Buildings/House/House_Red.png" width="128" height="192"/>
+ </tile>
+ <tile id="11">
+  <image source="../sprites/Factions/Knights/Buildings/House/House_Yellow.png" width="128" height="192"/>
+ </tile>
+ <tile id="12">
+  <image source="../sprites/Factions/Knights/Buildings/Tower/Tower_Blue.png" width="128" height="256"/>
+ </tile>
+ <tile id="13">
+  <image source="../sprites/Factions/Knights/Buildings/Tower/Tower_Construction.png" width="128" height="256"/>
+ </tile>
+ <tile id="14">
+  <image source="../sprites/Factions/Knights/Buildings/Tower/Tower_Destroyed.png" width="128" height="256"/>
+ </tile>
+ <tile id="15">
+  <image source="../sprites/Factions/Knights/Buildings/Tower/Tower_Purple.png" width="128" height="256"/>
+ </tile>
+ <tile id="16">
+  <image source="../sprites/Factions/Knights/Buildings/Tower/Tower_Red.png" width="128" height="256"/>
+ </tile>
+ <tile id="17">
+  <image source="../sprites/Factions/Knights/Buildings/Tower/Tower_Yellow.png" width="128" height="256"/>
+ </tile>
+</tileset>
---
+++ assets/tilesets/Building_Monster.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Building_Monster" tilewidth="1024" tileheight="192" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../sprites/Factions/Goblins/Buildings/Wood_House/Goblin_House.png" width="128" height="192"/>
+ </tile>
+ <tile id="1">
+  <image source="../sprites/Factions/Goblins/Buildings/Wood_House/Goblin_House_Destroyed.png" width="128" height="192"/>
+ </tile>
+ <tile id="2">
+  <image source="../sprites/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Blue.png" width="1024" height="192"/>
+ </tile>
+ <tile id="3">
+  <image source="../sprites/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Destroyed.png" width="256" height="192"/>
+ </tile>
+ <tile id="4">
+  <image source="../sprites/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_InConstruction.png" width="256" height="192"/>
+ </tile>
+ <tile id="5">
+  <image source="../sprites/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Purple.png" width="1024" height="192"/>
+ </tile>
+ <tile id="6">
+  <image source="../sprites/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Red.png" width="1024" height="192"/>
+ </tile>
+ <tile id="7">
+  <image source="../sprites/Factions/Goblins/Buildings/Wood_Tower/Wood_Tower_Yellow.png" width="1024" height="192"/>
+ </tile>
+</tileset>
