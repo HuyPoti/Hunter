c0cabd52e020b1f105d482aec4855144a3030627
HuyPoti <huynguyengiapoti@gmail.com>
2025-04-27 20:23:42 +0700

    map 1, 2, 3 don't test

---
+++ assets/tilesets/Foam.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Foam" tilewidth="80" tileheight="83" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <tile id="3">
+  <image source="../sprites/Terrain/Water/Foam/Foam_1frame.png" width="80" height="83"/>
+ </tile>
+</tileset>
---
+++ assets/tilesets/GoldMine.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="GoldMine" tilewidth="192" tileheight="128" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../sprites/Resources/Gold Mine/GoldMine_Active.png" width="192" height="128"/>
+ </tile>
+ <tile id="1">
+  <image source="../sprites/Resources/Gold Mine/GoldMine_Destroyed.png" width="192" height="128"/>
+ </tile>
+ <tile id="2">
+  <image source="../sprites/Resources/Gold Mine/GoldMine_Inactive.png" width="192" height="128"/>
+ </tile>
+</tileset>
